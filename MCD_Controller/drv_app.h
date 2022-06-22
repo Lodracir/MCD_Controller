dd8cb2285362bca12b4bf661742236898734b2a2
Lodracir <rescarcega21@outlook.com>
2022-06-22 17:47:12 -0600

    New Libraries Added (V0,1b)

---
+++ MCD_Controller/drv_app.h
@@ -0,0 +1,16 @@
+#ifndef _DRV_APP_H_
+#define _DRV_APP_H_
+
+/* Board Include */
+#include "board.h"
+
+/* Peripheral Includes */
+#include "drv_digital_in.h"
+#include "drv_digital_out.h"
+#include "drv_analog_in.h"
+#include "drv_uart.h"
+
+/* Prototype Functions */
+void SystemConfig(void);
+
+#endif // _DRV_APP_H_
--- MCD_Controller/main.c
+++ MCD_Controller/main.c
@@ -3,11 +3,19 @@
  * @brief Main function for MCD_Controller application.
  */
 
+/* Main Include */
+#include "main.h"
+
 int main(void)
 {
+    
+    /* Init System */
+    SystemConfig();
+    
     /* Replace with your application code */
     while (1)
     {
+        
     }
 
     return 0;
