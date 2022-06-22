a6b90c877097773e6a261e237b4811ace8825d35
Lodracir <rescarcega21@outlook.com>
2022-06-22 16:17:22 -0600

    Added main project files

---
+++ MCD_Controller/main.c
@@ -0,0 +1,14 @@
+/**
+ * @file main.c
+ * @brief Main function for MCD_Controller application.
+ */
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1)
+    {
+    }
+
+    return 0;
+}
